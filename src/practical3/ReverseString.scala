40cffad96879a71cc8c362d8b4efadfff058f0c4
Upeka <chamudiupeka20@gmail.com>
2024-07-15 10:54:18 +0530

    practical3-3

---
+++ src/practical3/ReverseString.scala
@@ -0,0 +1,17 @@
+package practical3
+
+object ReverseString {
+  def main(args: Array[String]): Unit = {
+
+    val str = "Scala"
+
+    def reverseString(s: String): String = {
+      if (s.isEmpty) ""
+      else reverseString(s.tail) + s.head
+    }
+
+    val reversedStr = reverseString(str)
+    println(reversedStr)
+      
+  }
+}
