1b836f73171350dcd87dfcffbab8d833e83b3abd
Upeka <chamudiupeka20@gmail.com>
2024-07-15 10:54:45 +0530

    practical3-4

---
+++ src/practical3/SumOfEvenNumbers.scala
@@ -0,0 +1,15 @@
+package practical3
+
+object SumOfEvenNumbers {
+  def main(args: Array[String]): Unit = {
+
+    def sumOfEvenNumbers(numbers: List[Int]): Int = {
+      numbers.filter(_ % 2 == 0).sum
+    }
+
+    val numbers = List(1, 2, 3, 4, 5, 6)
+    val sumEven = sumOfEvenNumbers(numbers)
+    println(sumEven)
+
+  }
+}
