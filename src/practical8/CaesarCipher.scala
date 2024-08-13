ac994a35657367a7dcadc8ba4aebb603c0ae85cf
Upeka <chamudiupeka20@gmail.com>
2024-08-13 06:29:19 +0530

    CaesarCipher update1

--- src/practical8/CaesarCipher.scala
+++ src/practical8/CaesarCipher.scala
@@ -2,24 +2,24 @@ package practical8
 
 object CaesarCipher {
 
-  // Function to encrypt the plaintext using Caesar cipher
+
   def encrypt(plaintext: String, shift: Int): String = {
     plaintext.map { char =>
       if (char.isLetter) {
         val base = if (char.isLower) 'a' else 'A'
         ((char - base + shift) % 26 + base).toChar
       } else {
-        char // Non-letter characters are not encrypted
+        char
       }
     }
   }
 
-  // Function to decrypt the ciphertext using Caesar cipher
+
   def decrypt(ciphertext: String, shift: Int): String = {
-    encrypt(ciphertext, 26 - shift) // Decryption is just encryption with 26 - shift
+    encrypt(ciphertext, 26 - shift)
   }
 
-  // General cipher function which takes an encryption or decryption function
+
   def cipher(text: String, shift: Int, algo: (String, Int) => String): String = {
     algo(text, shift)
   }
@@ -30,11 +30,11 @@ object CaesarCipher {
 
 
 
-    // Encrypt the plaintext
+
     val encryptedText = cipher(plaintext, shift, encrypt)
     println(s"Encrypted: $encryptedText")
 
-    // Decrypt the ciphertext
+
     val decryptedText = cipher(encryptedText, shift, decrypt)
     println(s"Decrypted: $decryptedText")
   }
